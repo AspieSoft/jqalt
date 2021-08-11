0c1164b0c656b123f53ee6d70c58549f8ea2bd1e
AspieSoft <aspiesoftweb@gmail.com>
2021-08-11 09:28:15 -0400

    added ability to detect that module is not jquery

--- jqalt/index.js
+++ jqalt/index.js
@@ -2,5 +2,7 @@ import {$} from './selector.js'
 import './common.js'
 import './class-functions/all.js'
 
-
 window.$ = Object.freeze($);
+
+// to allow detection of this module vs jquery
+window.jqAlt = Object.freeze($);
